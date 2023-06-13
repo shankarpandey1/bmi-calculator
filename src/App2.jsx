4722060959439bbbd568d3ffc559d1c8d9008df8
Shankar <drspvet@gmail.com>
2023-06-12 21:09:50 -0400

    yes

---
+++ src/App2.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import Header from './components/Header';
+import './app.scss';
+import Result2 from './components/Result2';
+import BmiForm2 from './components/BmiForm2';
+
+
+function App2() {
+  
+const [state, setState] = useState({
+    height: " ",
+    weight: " ",
+    result: " ",
+});
+
+
+  return (
+    <div class="container">
+     
+      <Header />
+      <BmiForm2 
+     
+      state = {state}
+      setState = {setState}
+      
+      />
+
+       <Result2
+       state = {state}
+       />
+      
+    </div>
+  );
+}
+
+export default App2;
